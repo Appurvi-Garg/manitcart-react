f89def9e3d179d84938e2e946bfc3bb2870e11b7
Appurvi Garg <gargappurvi2004@gmail.com>
2026-06-23 22:33:21 +0530

    first get api works

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const products = [
   {
@@ -22,7 +23,18 @@ app.get("/", (req, res) => {
 app.get("/products", (req, res) => {
   res.json(products);
 });
+app.post("/products", (req, res) => {
 
+  const newProduct = req.body;
+
+  products.push(newProduct);
+
+  res.json({
+    message: "Product Added",
+    product: newProduct
+  });
+
+});
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
